d16cfa9fc8ff20abd3159d5c494c13fd6c67d61b
Christian Franzen <christian.franzen@inform-software.com>
2022-02-08 23:51:41 +0100

    Added API gateway

---
+++ apigw/src/main/java/de/cfranzen/apigw/ApiGwApplication.java
@@ -0,0 +1,13 @@
+package de.cfranzen.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGwApplication.class, args);
+    }
+}
