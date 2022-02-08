135936822ef670ffb33e3abdbeafd14d96800e08
Christian Franzen <christian.franzen@inform-software.com>
2022-02-08 09:55:19 +0100

    Added service discovery using eureka

--- customer/src/main/java/de/cfranzen/customer/CustomerService.java
+++ customer/src/main/java/de/cfranzen/customer/CustomerService.java
@@ -16,7 +16,7 @@ class CustomerService {
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
         repository.saveAndFlush(customer);
 
-        FraudCheckResponse response = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        FraudCheckResponse response = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
         if (response.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
